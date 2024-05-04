f3d432b5bc8beb492f6f97b0870d13f5a87d14fa
ehsanh2001 <ehsanh2001@gmail.com>
2024-05-04 12:02:31 -0400

    Add Text class and the tests

---
+++ test/text.test.js
@@ -0,0 +1,24 @@
+const SVG = require("../lib/svg");
+const Text = require("../lib/text");
+
+describe("Text", () => {
+  test("constructor", () => {
+    const text = new Text("red", "ABC", 50);
+    expect(text.color).toBe("red");
+    expect(text.text).toBe("ABC");
+    expect(text.fontSize).toBe(50);
+    expect(text.svgParent).toBe(null);
+  });
+
+  test("should render the text in the middle of the parent", () => {
+    const svg = new SVG(300, 200);
+    const text = new Text("blue", "ABC");
+    svg.add(text);
+
+    const result = text.render();
+
+    expect(result).toBe(
+      '<text text-anchor="middle" x="150" y="120" font-size="60" fill="blue">ABC</text>'
+    );
+  });
+});
